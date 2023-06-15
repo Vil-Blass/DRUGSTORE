56e97460df2cd53f29780b7a440727f0efde1de3
Vil_Blass <jeffry16snake@hotmail.com>
2023-06-15 15:33:35 +0200

    empezando con el header

---
+++ scriptjj/main.js
@@ -0,0 +1,22 @@
+//mostrar la hora y la fecha local en fechaDia
+const fechaDia =document.querySelector('#fechaDia')
+// funcion de relog automatico
+setInterval(()=>{
+    const fecha = new Date()
+    const diaSemana= fecha.getDay()
+    const diaSemanaTexto = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado']
+    const dia = fecha.getDate()
+    const mes = fecha.getMonth()
+    const año = fecha.getFullYear()
+    const hora = fecha.getHours()
+    const minutos = fecha.getMinutes()
+    const segundos = fecha.getSeconds()
+    const ampm = hora >= 12 ? 'PM' : 'AM'
+    const hora12 = (hora % 12) || 12
+    const horaAMPM = hora12 + ':' + minutos + ':' + segundos + ' ' + ampm
+    fechaDia.innerHTML = `${diaSemanaTexto[diaSemana]}, ${dia} de ${mes} del ${año} a las ${horaAMPM}`
+})
+
+
+
+
