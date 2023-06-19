bd7a75ec4a89521d165964735244f4401f60684e
Vil_Blass <jeffry16snake@hotmail.com>
2023-06-19 14:22:26 +0200

    modal log in

--- scriptjj/main.js
+++ scriptjj/main.js
@@ -18,50 +18,145 @@ setInterval(() => {
 })
 
 listado = document.querySelectorAll('.listado')
-listado.forEach((elemento) => {
-    elemento.addEventListener('click', () => {
-        elemento.classList.toggle('active')
+listado.forEach(element => {
+    element.addEventListener('click', () => {
+        element.classList.toggle('active')
     })
 })
 
-
 // listado zumos pago
 
-//crear carousel automatico con el array 
-let carousel = document.getElementById('carousel');
-let img = document.createElement('img');
-img.setAttribute('id', 'img');
-img.setAttribute('class', 'img');
-img.setAttribute('src', juegos[0]);
-carousel.appendChild(img);
- img.style.height = '100%';
+// //crear carousel automatico con el array 
+// let carousel = document.getElementById('carousel');
+// let img = document.createElement('img');
+// img.setAttribute('id', 'img');
+// img.setAttribute('class', 'img');
+// img.setAttribute('src', juegos[0]);
+// carousel.appendChild(img);
+//  img.style.height = '100%';
 
-//cambiar imagenes automaticamente
-let i = 0;
-setInterval( ()=> {
-    if (i < juegos.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-    img.setAttribute('src', juegos[i]);
-}
-    , 2500);
-// dar tamaños
-img.style.width = '80%';
-img.style.height = '100%';
-//animar y dar estilo
-img.addEventListener('mouseover', function () {
-    img.style.transform = 'scale(1.2)';
-    img.style.transition = 'all 1s';
-}
-);
-img.addEventListener('mouseout', function () {
-    img.style.transform = 'scale(1)';
-    img.style.transition = 'all 1s';
-}
-);
+// //cambiar imagenes automaticamente
+// let i = 0;
+// setInterval( ()=> {
+//     if (i < juegos.length - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+//     img.setAttribute('src', juegos[i]);
+// }
+//     , 2500);
+// // dar tamaños
+// img.style.width = '80%';
+// img.style.height = '100%';
+// //animar y dar estilo
+// img.addEventListener('mouseover', function () {
+//     img.style.transform = 'scale(1.2)';
+//     img.style.transition = 'all 1s';
+// }
+// );
+// img.addEventListener('mouseout', function () {
+//     img.style.transform = 'scale(1)';
+//     img.style.transition = 'all 1s';
+// }
+// );
 console.log('ESTOY')
+
+// crear un array para nuevo usuario
+let usuarios = [];
+
+// registrar usuaros 
+function registrar() {
+    let nombre = document.querySelector('.last')
+    let apellidos = document.querySelector('.first')
+    let años = document.querySelector('.years')
+    let email = document.querySelector('.email')
+    let password = document.querySelector('.pass')
+    let check = document.querySelector('.check')
+    usuarios.push({
+        nombre: nombre.value,
+        apellidos: apellidos.value,
+        años: años.value,
+        email: email.value,
+        password: password.value
+    });
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios))
+    console.log(usuarios)
+    //requerir check
+    if (check.checked) {
+        localStorage.setItem('usuarios', JSON.stringify(usuarios))
+    }
+    else {
+        localStorage.removeItem('usuarios')
+    }
+    let envio =document.querySelector('.envio')
+    envio.addEventListener('click', () => {
+        //index
+        window.location.href = 'index.html'
+    })
+    alert('Usuario registrado correctamente')
+}
+
+
+
+// crear funcion para logearse
+function logIn() {
+    //crear modal para usuarii y contraseña
+    let modal = document.createElement('div')
+    modal.setAttribute('class', 'modal')
+    let div = document.createElement('div')
+    div.setAttribute('class', 'modal-content')
+    let p = document.createElement('p')
+    p.setAttribute('class', 'close')
+    p.innerHTML = 'X'
+    modal.appendChild(div)
+    div.appendChild(p)
+
+    document.head.appendChild(modal)
+    // crear input usuario
+    let usuario = document.createElement('input')
+    usuario.setAttribute('required', 'true')
+    usuario.setAttribute('type', 'text')
+    usuario.setAttribute('placeholder', 'Usuario')
+    div.appendChild(usuario)
+    // crear input contraseña
+    let password = document.createElement('input')
+    password.setAttribute('required', 'true')
+    password.setAttribute('type', 'password')
+    password.setAttribute('placeholder', 'Contraseña')
+    div.appendChild(password)
+    // crear boton
+    let boton = document.createElement('button')
+    boton.setAttribute('type', 'submit')
+    boton.setAttribute('class', 'btn')
+    boton.innerHTML = 'Ingresar'
+    div.appendChild(boton)
+    boton.addEventListener('click', () => {
+        // su usuario y contraseca coincide
+        if (usuario.value == usuarios[0].email && password.value == usuarios[0].password) {
+            window.location.href = 'index.html'
+        }
+        else {
+            alert('Usuario o contraseña incorrectos')
+        }
+
+    })
+
+}
+
+let entrar = document.querySelector('.entrar')
+entrar.addEventListener('click', logIn)
+
+
+
+
+
+
+
+
+
+
 
 
 
